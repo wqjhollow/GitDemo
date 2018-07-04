a836a0ddaa9c07821bfa6a3bfa682f8e7ce4a9ff
Qijing <18630631143@163.com>
2018-07-04 15:47:35 +0800

    Test

--- app/src/main/java/cn/com/aloha/gitdemo/MainActivity.kt
+++ app/src/main/java/cn/com/aloha/gitdemo/MainActivity.kt
@@ -8,5 +8,8 @@ class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val test = Test()
+        test.test()
     }
 }
---
+++ app/src/main/java/cn/com/aloha/gitdemo/Test.kt
@@ -0,0 +1,9 @@
+package cn.com.aloha.gitdemo
+
+import android.util.Log
+
+class Test {
+    fun test(){
+        Log.e("","Hello World!")
+    }
+}
